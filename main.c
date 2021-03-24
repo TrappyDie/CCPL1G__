576a1f973c48025565e74ec574f6bcc946f2e07c
André Costa <74073671+TrappyDie@users.noreply.github.com>
2021-03-24 15:38:44 +0000

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,35 @@ int main(void) {
     long n;
     char c[100];
     int i = 0;
+    STACK s1 = criar_stack()
+    STACK s2 = criar_stack()
 
     fgets(val, 100, stdin);
     while(sscanf(val, "%s%[^\n]", token, resto) > 0) {
         *resto = 0;
-        if(sscanf(token, "%ld", &n) == 1)
+        if (sscanf(token, "%ld", &n) == 1)
+        	PUSH (s1,n);
         else
             (c[i] = token[0]) && i++;
         c[i] = '\0';
     }
+    for(int x = strlen(c); x >= 0; x--){
+    	PUSH(s2,c[x]);
+    }
+    switch ('+') {
+    	case '+' : SUM(s1); break;
+    	case '-' : LESS(s1); break;
+    	case '/' : DIV(s1); break;
+    	case '*' : MULT(s1); break;
+    	case '#' : EXP(s1); break;
+    	case '%' : RES(s1); break;
+    	case '(' : INC(s1); break;
+    	case ')' : DEC(s1); break;
+    	case '&' : AND(s1); break;
+    	case '|' : OR(s1); break;
+    	case '^' : XOR(s1); break;
+    	case '~' : NOT(s1); break;
+    }
+
     return 0;
 }
