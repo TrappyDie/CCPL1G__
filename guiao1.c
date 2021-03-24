706230269393158b21d7212eece23eaf2387d395
André Costa <74073671+TrappyDie@users.noreply.github.com>
2021-03-24 14:43:47 +0000

    Create guiao1.c

---
+++ guiao1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char val[100];
+    char token[100];
+    char resto[100];
+    long n;
+    char c[100];
+    int i = 0;
+
+    fgets(val, 100, stdin);
+    while(sscanf(val, "%s%[^\n]", token, resto) > 0) {
+        *resto = 0;
+        if(sscanf(token, "%ld", &n) == 1)
+        else
+            (c[i] = token[0]) && i++;
+        c[i] = '\0';
+    }
+    return 0;
+}
